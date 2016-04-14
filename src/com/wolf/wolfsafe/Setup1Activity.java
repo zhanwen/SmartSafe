6af07ce373b7c348ab303ce290ac3ca7f793df3d
hanwen <1106002609@qq.com>
2016-04-14 22:16:16 +0800

    防盗页面设置向导一

---
+++ src/com/wolf/wolfsafe/Setup1Activity.java
@@ -0,0 +1,16 @@
+package com.wolf.wolfsafe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Setup1Activity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_setup1);
+		
+	}
+	
+}
