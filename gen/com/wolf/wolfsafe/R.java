38bb13369d6c8dabad84f69bb7f5ad43313b5cb4
hanwen <1106002609@qq.com>
2016-04-14 22:09:40 +0800

    temp

--- gen/com/wolf/wolfsafe/R.java
+++ gen/com/wolf/wolfsafe/R.java
@@ -1095,15 +1095,37 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int app=0x7f020057;
         public static final int atools=0x7f020058;
-        public static final int callmsgsafe=0x7f020059;
-        public static final int ic_launcher=0x7f02005a;
-        public static final int lunch_bg=0x7f02005b;
-        public static final int netmanager=0x7f02005c;
-        public static final int safe=0x7f02005d;
-        public static final int settings=0x7f02005e;
-        public static final int sysoptimize=0x7f02005f;
-        public static final int taskmanager=0x7f020060;
-        public static final int trojan=0x7f020061;
+        public static final int bind=0x7f020059;
+        public static final int btn_green_normal=0x7f02005a;
+        public static final int btn_green_pressed=0x7f02005b;
+        public static final int button=0x7f02005c;
+        public static final int button_normal=0x7f02005d;
+        public static final int button_other=0x7f02005e;
+        public static final int button_pressed=0x7f02005f;
+        public static final int button_select=0x7f020060;
+        public static final int callmsgsafe=0x7f020061;
+        public static final int comm_btn_bg2_n=0x7f020062;
+        public static final int comm_btn_bg2_s=0x7f020063;
+        public static final int function_greenbutton_normal=0x7f020064;
+        public static final int function_greenbutton_pressed=0x7f020065;
+        public static final int ic_launcher=0x7f020066;
+        public static final int listview_devider=0x7f020067;
+        public static final int lock=0x7f020068;
+        public static final int lunch_bg=0x7f020069;
+        public static final int netmanager=0x7f02006a;
+        public static final int next=0x7f02006b;
+        public static final int phone=0x7f02006c;
+        public static final int presence_invisible=0x7f02006d;
+        public static final int presence_online=0x7f02006e;
+        public static final int previous=0x7f02006f;
+        public static final int safe=0x7f020070;
+        public static final int settings=0x7f020071;
+        public static final int setup1=0x7f020072;
+        public static final int star_big_on=0x7f020073;
+        public static final int sysoptimize=0x7f020074;
+        public static final int taskmanager=0x7f020075;
+        public static final int trojan=0x7f020076;
+        public static final int unlock=0x7f020077;
     }
     public static final class id {
         public static final int action_bar=0x7f09001c;
@@ -1117,11 +1139,12 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f090002;
         public static final int action_menu_presenter=0x7f090003;
         public static final int action_mode_close_button=0x7f090024;
-        public static final int action_settings=0x7f090047;
+        public static final int action_settings=0x7f09004b;
         public static final int activity_chooser_view_content=0x7f090025;
         public static final int always=0x7f09000f;
         public static final int beginning=0x7f090016;
-        public static final int cb_status=0x7f090046;
+        public static final int cancel=0x7f090044;
+        public static final int cb_status=0x7f09004a;
         public static final int checkbox=0x7f09002d;
         public static final int collapseActionView=0x7f090010;
         public static final int default_activity_button=0x7f090028;
@@ -1130,6 +1153,8 @@ containing a value of this type.
         public static final int dropdown=0x7f090015;
         public static final int edit_query=0x7f090030;
         public static final int end=0x7f090017;
+        public static final int et_setup_confirm=0x7f090045;
+        public static final int et_setup_pwd=0x7f090042;
         public static final int expand_activities_button=0x7f090026;
         public static final int expanded_menu=0x7f09002c;
         public static final int home=0x7f090000;
@@ -1137,7 +1162,7 @@ containing a value of this type.
         public static final int icon=0x7f09002a;
         public static final int ifRoom=0x7f090011;
         public static final int image=0x7f090027;
-        public static final int iv_item=0x7f090042;
+        public static final int iv_item=0x7f090046;
         public static final int listMode=0x7f090006;
         public static final int list_home=0x7f09003d;
         public static final int list_item=0x7f090029;
@@ -1145,6 +1170,7 @@ containing a value of this type.
         public static final int never=0x7f090012;
         public static final int none=0x7f090019;
         public static final int normal=0x7f090007;
+        public static final int ok=0x7f090043;
         public static final int progress_circular=0x7f090004;
         public static final int progress_horizontal=0x7f090005;
         public static final int radio=0x7f09002f;
@@ -1170,10 +1196,10 @@ containing a value of this type.
         public static final int textView1=0x7f09003c;
         public static final int title=0x7f09002b;
         public static final int top_action_bar=0x7f090020;
-        public static final int tv_desc=0x7f090045;
-        public static final int tv_item=0x7f090043;
+        public static final int tv_desc=0x7f090049;
+        public static final int tv_item=0x7f090047;
         public static final int tv_splash_version=0x7f090040;
-        public static final int tv_title=0x7f090044;
+        public static final int tv_title=0x7f090048;
         public static final int tv_update_info=0x7f090041;
         public static final int up=0x7f090021;
         public static final int useLogo=0x7f09000e;
@@ -1230,12 +1256,15 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_home=0x7f030018;
-        public static final int activity_setting=0x7f030019;
-        public static final int activity_splash=0x7f03001a;
-        public static final int dialog_setup_password=0x7f03001b;
-        public static final int list_item_home=0x7f03001c;
-        public static final int setting_item_view=0x7f03001d;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001e;
+        public static final int activity_lost_find=0x7f030019;
+        public static final int activity_setting=0x7f03001a;
+        public static final int activity_setup1=0x7f03001b;
+        public static final int activity_splash=0x7f03001c;
+        public static final int dialog_enter_password=0x7f03001d;
+        public static final int dialog_setup_password=0x7f03001e;
+        public static final int list_item_home=0x7f03001f;
+        public static final int setting_item_view=0x7f030020;
+        public static final int support_simple_spinner_dropdown_item=0x7f030021;
     }
     public static final class menu {
         public static final int splash=0x7f0c0000;
@@ -1290,11 +1319,13 @@ containing a value of this type.
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
         
 
         Base application theme for API 11+. This theme completely replaces
@@ -1515,6 +1546,7 @@ containing a value of this type.
         public static final int Widget_AppCompat_ProgressBar=0x7f0b000a;
         public static final int Widget_AppCompat_ProgressBar_Horizontal=0x7f0b0009;
         public static final int Widget_AppCompat_Spinner_DropDown_ActionBar=0x7f0b0022;
+        public static final int text_content_style=0x7f0b008d;
     }
     public static final class styleable {
         /**  ============================================ 
