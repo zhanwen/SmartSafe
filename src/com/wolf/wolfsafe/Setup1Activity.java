e47637ef4669541614b3a443bbc21f207702079c
hanwen <1106002609@qq.com>
2016-04-15 22:21:33 +0800

    步骤一页面

--- src/com/wolf/wolfsafe/Setup1Activity.java
+++ src/com/wolf/wolfsafe/Setup1Activity.java
@@ -1,15 +1,40 @@
 package com.wolf.wolfsafe;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.View;
 
-public class Setup1Activity extends Activity {
+public class Setup1Activity extends BaseSetupActivity {
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
 		setContentView(R.layout.activity_setup1);
+		
+	}
+	
+	@Override
+	public void showNext() {
+		Intent intent = new Intent(Setup1Activity.this,Setup2Activity.class);
+		startActivity(intent);
+		//关闭当前页
+		finish();
+		
+		/**
+		 * 要求在finish（）或者startActivity（intent）；后面执行
+		 */
+		overridePendingTransition(R.anim.tran_in, R.anim.tran_out);
+	}
+	
+	
+	@Override
+	public void showPre() {
+		// TODO Auto-generated method stub
 		
 	}
 	
