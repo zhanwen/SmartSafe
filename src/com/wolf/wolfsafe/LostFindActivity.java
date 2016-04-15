b325a5c5e7f9628a84059c418e465a792092bbdd
hanwen <1106002609@qq.com>
2016-04-15 22:20:03 +0800

    防盗页面

--- src/com/wolf/wolfsafe/LostFindActivity.java
+++ src/com/wolf/wolfsafe/LostFindActivity.java
@@ -4,12 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LostFindActivity extends Activity {
 	
 	private SharedPreferences sp;
-	
-	
+	private TextView tv_safenumber;
+	private ImageView iv_protecting;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,6 +23,23 @@ public class LostFindActivity extends Activity {
 		if(configed) {
 			//就在手机防盗页面
 			setContentView(R.layout.activity_lost_find);
+			tv_safenumber = (TextView) findViewById(R.id.tv_safenumber);
+			iv_protecting = (ImageView) findViewById(R.id.iv_protecting);
+			
+			//得到我们设置的安全号码
+			String safenumber = sp.getString("safenumber", "");
+			tv_safenumber.setText(safenumber);
+			
+			//设置防盗保护状态
+			boolean protecting = sp.getBoolean("protecting", false);
+			if(protecting) {
+				//已经开启防盗保护
+				iv_protecting.setImageResource(R.drawable.lock);
+			}else {
+				//没有开启防盗保护
+				iv_protecting.setImageResource(R.drawable.unlock);
+			}
+			
 		}else {
 			//还没有做过设置向导
 			Intent intent = new Intent(this, Setup1Activity.class);
@@ -27,8 +47,22 @@ public class LostFindActivity extends Activity {
 			//关闭当前页面
 			finish();
 		}
-		
-		
-		
 	}
+	
+	
+	/**
+	 * 重新进入手机防盗设置向导页面
+	 * @param view
+	 */
+	public void reEnterSetup(View view) {
+		Intent intent = new Intent(this, Setup1Activity.class);
+		startActivity(intent);
+		//关闭当前页面
+		finish();
+	}
+	
+	
+	
+	
+	
 }
