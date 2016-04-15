aef7b69c2becc88ec572db3b1f4e5728d2540394
hanwen <1106002609@qq.com>
2016-04-15 22:21:38 +0800

    步骤二页面

---
+++ src/com/wolf/wolfsafe/Setup2Activity.java
@@ -0,0 +1,99 @@
+package com.wolf.wolfsafe;
+
+import com.wolf.wolfsafe.ui.SettingItemView;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+public class Setup2Activity extends BaseSetupActivity {
+
+	private SettingItemView siv_setup2_sim;
+	/**
+	 * 读取手机sim卡的信息
+	 */
+	private TelephonyManager tm;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_setup2);
+		
+		siv_setup2_sim = (SettingItemView) findViewById(R.id.siv_setup2_sim);
+		tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+		String sim = sp.getString("sim", null);
+		if(TextUtils.isEmpty(sim)) {
+			//没有绑定
+			siv_setup2_sim.setChecked(false);
+		}else {
+			//已经绑定
+			siv_setup2_sim.setChecked(true);
+		}
+		
+		siv_setup2_sim.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Editor editor = sp.edit();
+				if(siv_setup2_sim.isChecked()) {
+					siv_setup2_sim.setChecked(false);
+					//保存sim卡的序列号
+					String sim = tm.getSimSerialNumber();
+					
+					editor.putString("sim", null);
+				} else {
+					siv_setup2_sim.setChecked(true);
+					//保存sim卡的序列号
+					String sim = tm.getSimSerialNumber();
+					editor.putString("sim", sim);
+				}
+				editor.commit();
+			}
+		});
+		
+	}
+	
+	/**
+	 * 下一步
+	 * @param view
+	 */
+	@Override
+	public void showNext() {
+		//取出是否绑定sim
+		String sim = sp.getString("sim", null);
+		if(TextUtils.isEmpty(sim)) {
+			//没有绑定
+			Toast.makeText(this, "sim卡没有绑定", 1).show();
+			return;
+		}
+			
+		Intent intent = new Intent(Setup2Activity.this,Setup3Activity.class);
+		startActivity(intent);
+		//关闭当前页
+		finish();
+		overridePendingTransition(R.anim.tran_in, R.anim.tran_out);
+	}
+	
+	/**
+	 * 上一步
+	 * @param view
+	 */
+	@Override
+	public void showPre() {
+		Intent intent = new Intent(Setup2Activity.this,Setup1Activity.class);
+		startActivity(intent);
+		//关闭当前页
+		finish();
+		
+		overridePendingTransition(R.anim.tran_pre_in, R.anim.tran_pre_out);
+		
+		
+	}
+	
+}
