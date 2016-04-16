45695fbc981e1ff6ff437d7943ba5aa96794a523
hanwen <1106002609@qq.com>
2016-04-16 19:50:12 +0800

    电话地址查询页面

--- src/com/wolf/wolfsafe/NumberAddressActivity.java
+++ src/com/wolf/wolfsafe/NumberAddressActivity.java
@@ -1,21 +1,31 @@
 package com.wolf.wolfsafe;
 
-import com.wolf.wolfsafe.db.dao.NumberAddressQueryUtils;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.wolf.wolfsafe.db.dao.NumberAddressQueryUtils;
 
 public class NumberAddressActivity extends Activity {
 	
 	private static final String TAG = "NumberAddressActivity";
 	private EditText ad_phone;
 	private TextView result;
+	/**
+	 * 系统提供的振动服务
+	 */
+	private Vibrator vibrator;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,38 @@ public class NumberAddressActivity extends Activity {
 		
 		ad_phone = (EditText) findViewById(R.id.ad_phone);
 		result = (TextView) findViewById(R.id.result);
+		vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+		ad_phone.addTextChangedListener(new TextWatcher() {
+			/**
+			 * 当文本发生变化的时候回调
+			 */
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				if(s != null && s.length() >= 3) {
+					//查询数据库，并且显示结果
+					String address = NumberAddressQueryUtils.queryNumber(s.toString());
+					result.setText(address);
+				}
+				
+			}
+			/**
+			 * 当文本发生变化之前回调
+			 */
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+			/**
+			 * 当文本发生变化之后回调
+			 */
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 	
 	
@@ -35,6 +77,25 @@ public class NumberAddressActivity extends Activity {
 		String phone = ad_phone.getText().toString().trim();
 		if(TextUtils.isEmpty(phone)) {
 			Toast.makeText(this, "号码为空", 0).show();
+			Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+			
+//			shake.setInterpolator(new Interpolator() {
+//				
+//				@Override
+//				public float getInterpolation(float x) {
+//					//函数的运算
+//					return y;
+//				}
+//			});
+			
+			ad_phone.startAnimation(shake);
+			
+			//当电话号码为空的时候，就去振动手机提醒用户
+//			vibrator.vibrate(2000);
+			long[] pattern = {200,200,300,300,1000,2000};
+			//-1表示不重复,0循环振动,1的话从数组第几个开始
+			vibrator.vibrate(pattern, -1);
+
 			return;
 		}else {
 			String address = NumberAddressQueryUtils.queryNumber(phone);
