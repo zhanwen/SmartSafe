a43ca8e15a14fd1bec78ba6ede688762244f51e0
hanwen <1106002609@qq.com>
2016-04-16 19:50:46 +0800

    OutCallReceiver

---
+++ src/com/wolf/wolfsafe/receiver/OutCallReceiver.java
@@ -0,0 +1,21 @@
+package com.wolf.wolfsafe.receiver;
+
+import com.wolf.wolfsafe.db.dao.NumberAddressQueryUtils;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class OutCallReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+//		//得到打出去的电话号码,
+//		String phone = getResultData();
+//		//得到号码查询号码归属地
+//		String address = NumberAddressQueryUtils.queryNumber(phone);
+//		Toast.makeText(context, address, 1).show();
+	}
+
+}
