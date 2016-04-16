47b40a50709eb278ca2413a7163f6578690bd614
hanwen <1106002609@qq.com>
2016-04-16 19:49:56 +0800

    电话地址查询辅助类

--- src/com/wolf/wolfsafe/db/dao/NumberAddressQueryUtils.java
+++ src/com/wolf/wolfsafe/db/dao/NumberAddressQueryUtils.java
@@ -14,23 +14,77 @@ public class NumberAddressQueryUtils {
 	 * @return
 	 */
 	public static String queryNumber(String number) {
-		
+
 		String address = number;
-		
 		// path 把address.db这个数据库拷贝到/data/data/<包名>/files/address.db
 
 		SQLiteDatabase database = SQLiteDatabase.openDatabase(path, null,
 				SQLiteDatabase.OPEN_READONLY);
 
-		Cursor cursor =  database.rawQuery(
-				"select location from data2 where id = (select outkey from data1 where id = ?)",
-				new String[] { number.substring(0, 7)});
-		
-		while(cursor.moveToNext()) {
-			String location = cursor.getString(0);
-			address = location;
+		// 手机号码13 14 15 16 18
+		// 手机号码的正则表达式
+		if (number.matches("^1[34568]\\d{9}$")) {
+			// 手机号码
+
+			Cursor cursor = database
+					.rawQuery(
+							"select location from data2 where id = (select outkey from data1 where id = ?)",
+							new String[] { number.substring(0, 7) });
+
+			while (cursor.moveToNext()) {
+				String location = cursor.getString(0);
+				address = location;
+			}
+			cursor.close();
+
+		} else {
+			// 其他的电话号码
+			switch (number.length()) {
+			case 3:
+				// 110
+				address = "绯警号码";
+				break;
+			case 4:
+				// 5558
+				address = "模拟器";
+				break;
+			case 5:
+				// 10086
+				address = "客服电话";
+				break;
+			case 7:
+				address = "本地号码";
+				break;
+			case 8:
+				address = "本地号码";
+				break;
+
+			default:// 处理长途电话 10
+				if (number.length() > 10 && number.startsWith("0")) {
+					// 010-59530359
+					Cursor cursor = database.rawQuery(
+							"select location from data2 where area = ?",
+							new String[] { number.substring(1, 3) });
+					while(cursor.moveToNext()) {
+						String location = cursor.getString(0);
+						address = location.substring(0, location.length()-2);
+					}
+					cursor.close();
+					
+					// 0855-59530359	
+					cursor = database.rawQuery(
+							"select location from data2 where area = ?",
+							new String[] { number.substring(1, 4) });
+					
+					while(cursor.moveToNext()) {
+						String location = cursor.getString(0);
+						address = location.substring(0, location.length()-2);
+					}
+					cursor.close();
+				}
+				break;
+			}
 		}
-		cursor.close();
 
 		return address;
 	}
