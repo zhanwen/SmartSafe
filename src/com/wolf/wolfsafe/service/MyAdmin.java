fc7a5b3b9693c4a56f6b6f9fb19512b92e11eaef
hanwen <1106002609@qq.com>
2016-04-16 12:14:46 +0800

    一键锁屏对应的

---
+++ src/com/wolf/wolfsafe/service/MyAdmin.java
@@ -0,0 +1,14 @@
+package com.wolf.wolfsafe.service;
+
+import android.app.admin.DeviceAdminReceiver;
+
+
+/**
+ * 特殊的广播接收者
+ * @author Hanwen
+ *
+ */
+public class MyAdmin extends DeviceAdminReceiver {
+	
+	
+}
