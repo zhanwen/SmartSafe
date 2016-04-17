0b70b123c03f3020c4a773415faee10174bdfe81
hanwen <1106002609@qq.com>
2016-04-17 22:09:14 +0800

    增加移动归属地的位置功能

--- src/com/wolf/wolfsafe/service/AddressService.java
+++ src/com/wolf/wolfsafe/service/AddressService.java
@@ -6,15 +6,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.graphics.Color;
+import android.content.SharedPreferences.Editor;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
+import android.os.SystemClock;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.wolf.wolfsafe.R;
 import com.wolf.wolfsafe.db.dao.NumberAddressQueryUtils;
@@ -38,6 +44,8 @@ public class AddressService extends Service {
 	
 	private View view;
 
+	private SharedPreferences sp;
+	private long[] mHits = new long[2];
 	@Override
 	public void onCreate() {
 		super.onCreate();
@@ -56,7 +64,9 @@ public class AddressService extends Service {
 		wm = (WindowManager) getSystemService(WINDOW_SERVICE);
 
 	}
-
+	
+	private WindowManager.LayoutParams params;
+	
 	/**
 	 * 自定义土司
 	 * 
@@ -65,26 +75,109 @@ public class AddressService extends Service {
 	private void myToast(String address) {
 		view = View.inflate(this, R.layout.address_show, null);
 		TextView textView = (TextView) view.findViewById(R.id.tv_address);
+		
+		view.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Log.i(TAG,"控件被点击了=======");
+				System.arraycopy(mHits, 1, mHits, 0, mHits.length - 1);
+				mHits[mHits.length - 1] = SystemClock.uptimeMillis();
+				if(mHits[0] >= (SystemClock.uptimeMillis() - 500)) {
+					//双击居中了
+					params.x = wm.getDefaultDisplay().getWidth()/2 - view.getWidth()/2;
+					wm.updateViewLayout(view, params);
+					Editor editor = sp.edit();
+					editor.putInt("lastx", params.x);
+					editor.commit();
+				}
+			}
+		});
+		
+		//给View对象设置一个触摸的监听器
+		view.setOnTouchListener(new OnTouchListener() {
+			//定义手指的初始位置
+			int startX; 
+			int startY;
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				switch (event.getAction()) {
+				case MotionEvent.ACTION_DOWN: //手指按下屏幕 
+					startX = (int) event.getRawX();
+					startY = (int) event.getRawY();
+//					Log.i(TAG,"开始位置：" + startX + "," + startY);
+					Log.i(TAG,"手指摸到控件");
+					break;
+				case MotionEvent.ACTION_MOVE: //手指在屏幕移动
+					int newX = (int) event.getRawX();
+					int newY = (int) event.getRawY();
+					Log.i(TAG,"新的位置：" + newX + "," + newY);
+					int dx = newX - startX;
+					int dy = newY - startY;
+//					Log.i(TAG, "手指的偏移量：" + dx + "," + dy);
+//					Log.i(TAG, "更新ImageView的窗体的位置， 偏移量" + dx + "," + dy);
+					Log.i(TAG,"手指在控件上移动");
+					params.x += dx;
+					params.y += dy;
+					//考虑边界问题
+					if(params.x < 0) {
+						params.x = 0;
+					}
+					if(params.y < 0) {
+						params.y = 0;
+					}
+					if(params.x > wm.getDefaultDisplay().getWidth() - view.getWidth()) {
+						params.x = wm.getDefaultDisplay().getWidth() - view.getWidth();
+					}
+					if(params.y > wm.getDefaultDisplay().getHeight() - view.getHeight()) {
+						params.y = wm.getDefaultDisplay().getHeight() - view.getHeight();
+					}
+					wm.updateViewLayout(view, params);
+					//重新初始化手指的开始结束位置
+					startX = (int) event.getRawX();
+					startY = (int) event.getRawY();
+					
+					break;
+				case MotionEvent.ACTION_UP: //手指离开屏幕一瞬间
+					Log.i(TAG,"手指离开控件");
+					//记录控件距离屏幕左上角的坐标
+					Editor editor = sp.edit();
+					editor.putInt("lastx", params.x);
+					editor.putInt("lasty", params.y);
+					editor.commit();
+					
+					break;
+				}
+				return false; //事件处理完毕了，不要让父控件     父布局响应触摸事件了
+			}
+		});
+		
 		//"半透明","活力橙","卫士蓝","金属灰","苹果绿"
 		int[] ids = {R.drawable.call_locate_white,R.drawable.call_locate_orange,R.drawable.call_locate_blue,
 				R.drawable.call_locate_gray,R.drawable.call_locate_green};
 		
-		SharedPreferences sp = getSharedPreferences("config", MODE_PRIVATE);
+		sp = getSharedPreferences("config", MODE_PRIVATE);
 		textView.setBackgroundResource(ids[sp.getInt("which", 0)]);
 		textView.setText(address);
 		
 		//窗体的参数就设置好
-		WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+		params = new WindowManager.LayoutParams();
 		
 		params.height = WindowManager.LayoutParams.WRAP_CONTENT;
 		params.width = WindowManager.LayoutParams.WRAP_CONTENT;
 		
 		params.flags = WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
-                | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
                 | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE;
 		params.format = PixelFormat.TRANSLUCENT;
-		params.type = WindowManager.LayoutParams.TYPE_TOAST;
-
+		//android系统里面具有电话优先级的一种窗体类型,记得添加权限
+		params.type = WindowManager.LayoutParams.TYPE_PRIORITY_PHONE;
+		//与窗体左上角对齐
+		params.gravity = Gravity.TOP + Gravity.LEFT;
+		//指定窗体距离左边200，上边100像素
+		params.x = sp.getInt("lastx", 0);
+		params.y = sp.getInt("lasty", 0);
+		
+		
 		wm.addView(view, params);
 	}
 
