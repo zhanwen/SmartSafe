a9b4a4698914a963e9197b8da4f9966d01a6b335
hanwen <1106002609@qq.com>
2016-04-18 15:38:09 +0800

    黑名单号码bean

---
+++ src/com/wolf/wolfsafe/domain/BlackNumberInfo.java
@@ -0,0 +1,31 @@
+package com.wolf.wolfsafe.domain;
+
+/**
+ * 黑名单号码的业务bean
+ * @author 02
+ *
+ */
+public class BlackNumberInfo {
+	private String number;
+	private String mode;
+	
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getMode() {
+		return mode;
+	}
+	public void setMode(String mode) {
+		this.mode = mode;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "BlackNumberInfo [number=" + number + ",mode=" + "]";
+	}
+	
+}
