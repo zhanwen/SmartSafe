a880150f7d89fa32b018bb1a1fba10c12eafb092
hanwen <1106002609@qq.com>
2016-04-21 09:34:52 +0800

    弹出窗体的动画效果

--- src/com/wolf/wolfsafe/AppManagerActivity.java
+++ src/com/wolf/wolfsafe/AppManagerActivity.java
@@ -13,6 +13,10 @@ import android.text.format.Formatter;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.ScaleAnimation;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
 import android.widget.AdapterView;
@@ -144,13 +148,24 @@ public class AppManagerActivity extends Activity {
 				
 				View contentView = View.inflate(getApplicationContext(), R.layout.popup_app_item, null);
 				popupWindow = new PopupWindow(contentView, -2, -2);
-//				popupWindow.setBackgroundDrawable(new ColorDrawable(Color.RED));
+				//动画效果的播放必须要求窗体有背景颜色
+				//透明颜色也是颜色
+				popupWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 				int[] location = new int[2];
 				view.getLocationInWindow(location);
 				//在代码里面设置的宽高值 都是像素-----> dip
 				int dip = 60;
 				int px = DensityUtil.dip2px(getApplicationContext(), dip);
 				popupWindow.showAtLocation(parent, Gravity.LEFT | Gravity.TOP, px, location[1]);
+				
+				ScaleAnimation sa = new ScaleAnimation(0.3f, 1.0f, 0.3f, 1.0f, Animation.RELATIVE_TO_SELF, 0, Animation.RELATIVE_TO_SELF, 0.5f);
+				sa.setDuration(300);
+				AlphaAnimation aa = new AlphaAnimation(0.5f,1.0f);
+				aa.setDuration(300);
+				AnimationSet set = new AnimationSet(false);
+				set.addAnimation(sa);
+				set.addAnimation(aa);
+				contentView.startAnimation(set);
 			}
 		});
 	}
